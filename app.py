ff69645a503df15593424ffb968ff422a82c1760
giu <giulialopezmdo@gmail.com>
2025-10-23 21:27:00 -0600

    showing questions

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 from jinja2 import TemplateNotFound
 import hashlib
 
+import datetime
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = "8so138bs28d32s4wz3872s8ou6oqwo74368o283" 
 
@@ -72,7 +74,8 @@ def login():
 
         role = user['role'].strip().lower()
         if role == 'alumnx':
-            return redirect(url_for('alumnxs_inicio'))
+            return redirect(url_for('encuestas_alumnx'))
+
         elif role == 'docente':
             return redirect(url_for('teachers_inicio'))
         elif role == 'admin':
@@ -91,12 +94,33 @@ def encuestas_alumnx():
         return redirect(url_for('login'))
 
     user_id = session['user_id']
+    now = datetime.datetime.now()
 
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT f.id, f.title, f.description, f.id_docente, f.id_materia, f.start_at, f.end_at, f.active FROM form f WHERE f.active = 1 AND ((f.id_materia IS NOT NULL AND EXISTS (SELECT 1 FROM alumnx_materia am WHERE am.id_alumnx = %s AND am.id_course = f.id_materia)) OR (f.id_docente IS NOT NULL AND EXISTS (SELECT 1 FROM alumnx_materia am JOIN docente_materia dm ON dm.id_materia = am.id_course WHERE am.id_alumnx = %s AND dm.id_docente = f.id_docente))) ORDER BY f.start_at IS NULL, f.start_at DESC, f.id DESC", (user_id,user_id))
+    #cursor.execute("SELECT f.id, f.title, f.description, f.id_docente, f.id_materia, f.start_at, f.end_at, f.active FROM form f WHERE f.active = 1 AND ((f.id_materia IS NOT NULL AND EXISTS (SELECT 1 FROM alumnx_materia am WHERE am.id_alumnx = %s AND am.id_course = f.id_materia)) OR (f.id_docente IS NOT NULL AND EXISTS (SELECT 1 FROM alumnx_materia am JOIN docente_materia dm ON dm.id_materia = am.id_course WHERE am.id_alumnx = %s AND dm.id_docente = f.id_docente))) ORDER BY f.start_at IS NULL, f.start_at DESC, f.id DESC", (user_id,user_id))
+    cursor.execute("""
+        SELECT f.id, f.title, f.description, f.id_docente, f.id_materia, f.start_at, f.end_at, f.active
+        FROM form f
+        WHERE f.active = 1
+        AND ( (f.start_at IS NULL OR f.start_at <= %s) AND (f.end_at IS NULL OR f.end_at >= %s) )
+        AND (
+      -- forma 1: encuesta vinculada a materia y el alumno está inscrito
+      (f.id_materia IS NOT NULL AND EXISTS (
+          SELECT 1 FROM alumnx_materia am WHERE am.id_alumnx = %s AND am.id_course = f.id_materia
+      ))
+      OR
+      -- forma 2: encuesta vinculada a docente y ese docente imparte alguna materia en la que está el alumno
+      (f.id_docente IS NOT NULL AND EXISTS (
+          SELECT 1 FROM alumnx_materia am
+          JOIN docente_materia dm ON dm.id_materia = am.id_course
+          WHERE am.id_alumnx = %s AND dm.id_docente = f.id_docente
+      ))
+        )
+        ORDER BY f.start_at IS NULL, f.start_at DESC, f.id DESC
+""", (now, now, user_id, user_id))
     surveys = cursor.fetchall()
-    print("User ID:", user_id)
-    print("Surveys:", surveys)
+    """print("User ID:", user_id)
+    print("Surveys:", surveys)"""
 
     cursor.close()
 
@@ -104,18 +128,41 @@ def encuestas_alumnx():
     return render_template('alumnxs/inicio-alumnxs.html', error=error, encuestas=surveys)
 
 # Función para el botón "contestar encuesta" que obtiene las preguntas y respuestas de cada encuesta
-@app.route('/alumnxs/inicio-alumnxs/<int:id_encuesta>', methods=['GET', 'POST'])
+@app.route('/alumnxs/inicio-alumnxs/<int:id_encuesta>', methods=['GET'])
 def contestar_encuesta(id_encuesta):
     error = None
 
     if 'user_id' not in session:
         return redirect(url_for('login'))
-
-    user_id = session['user_id']
-
     cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT title FROM form WHERE id = %s", (id_encuesta, ))
+    form_title = cursor.fetchone()
+    cursor.execute("SELECT id, texto_pregunta, tipo FROM question WHERE id_form = %s ORDER BY id ASC", (id_encuesta, ))
+    questions = cursor.fetchall()
+
+    options = {}
+    for question in questions:
+        if question['tipo'] == 'multiple':
+            cursor.execute("SELECT id, choice_text FROM choice WHERE id_question = %s ORDER BY id ASC", (question['id'], ))
+            options[question['id']] = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template('alumnxs/encuesta.html', error=error, id_encuesta=id_encuesta, form_title=form_title, questions=questions, options=options)
+
+# Función para el botón "enviar respuestas" que registra las respuestas en la bd
+@app.route('/alumnxs/inicio-alumnxs', methods=['GET', 'POST'])
+def enviar_respuestas():
+    error = None
+
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    cursor = db.cursor(dictionary=True)
+    # ("INSERT INTO response (id_form, id_alumnx) VALUES (?, ?)")
+    # ("INSERT INTO answer (response_id, id_question, choice_id, texto_respuesta) VALUES (?, ?, ?, ?)");
     cursor.close()
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
